a88ffde5de5e1c28320e40d8c47b10bdc2b28c54
Frank Ilunga <ilungafrank3@gmail.com>
2023-07-20 13:21:02 +0300

    changes to api backend

--- frontend/bongo-music-awards/dist/spa/assets/AppLayout.c9308d63.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,a as r}from"./QLayout.77fa69cb.js";import{an as n,m as c,o as s,c as _,w as e,a as o}from"./index.34fc6f26.js";import"./QResizeObserver.0c8df38c.js";const f={};function p(i,u){const a=c("router-view");return s(),_(t,{view:"hHh LpR fFf"},{default:e(()=>[o(r,null,{default:e(()=>[o(a)]),_:1})]),_:1})}var v=n(f,[["render",p]]);export{v as default};
--- frontend/bongo-music-awards/dist/spa/assets/use-quasar.ce1de379.js
+++
@@ -1,1 +0,0 @@
-import{A as a,bf as r}from"./index.34fc6f26.js";function u(){return a(r)}export{u};
