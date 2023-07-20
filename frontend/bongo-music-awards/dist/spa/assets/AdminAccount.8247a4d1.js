8c3da907f80c95bfd19a83b12f8daccd0285eb8c
Frank Ilunga <ilungafrank3@gmail.com>
2023-07-20 08:18:06 +0300

    feat:build

---
+++ frontend/bongo-music-awards/dist/spa/assets/AdminAccount.8247a4d1.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./QPage.225bd69c.js";import{an as e,o as t,c as r,w as c,h as o}from"./index.34fc6f26.js";const s={};function n(_,d){return t(),r(a,{padding:""},{default:c(()=>[o(" artist works ")]),_:1})}var m=e(s,[["render",n]]);export{m as default};
